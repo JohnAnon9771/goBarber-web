3cf4797ee2abfaec333599df94e3fcbb4fa4ac8d
JohnAnon9771 <njoao97710@gmail.com>
2020-03-12 22:54:20 -0300

    added config for redux

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { Router } from 'react-router-dom';
 
 import './config/ReactotronConfig';
 
+import { Provider } from 'react-redux';
+import store from './store';
+
 import history from './services/history';
 import Routes from './routes';
 
@@ -10,10 +13,12 @@ import GlobalStyle from './styles/global';
 
 function App() {
   return (
-    <Router history={history}>
-      <Routes />
-      <GlobalStyle />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <Routes />
+        <GlobalStyle />
+      </Router>
+    </Provider>
   );
 }
 
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,18 +1,47 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
 import Input from '../../components/Input';
 
 import logo from '../../assets/logo.svg';
 
 export default function SignUp() {
-  function handleSubmit(data) {
-    return data;
+  const formRef = useRef(null);
+  async function handleSubmit(data, { reset }) {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome é obrigatório'),
+        email: Yup.string()
+          .email('E-mail inválido')
+          .required('E-mail é obrigatório'),
+        password: Yup.string()
+          .min(6, 'Senha deve conter no Minimo 6 caracteres')
+          .required('Senha obrigatória'),
+      });
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+      reset();
+      formRef.current.setErrors({});
+    } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        const errorMessages = {};
+
+        error.inner.forEach(err => {
+          errorMessages[err.path] = err.message;
+        });
+
+        formRef.current.setErrors(errorMessages);
+      }
+    }
   }
+
   return (
     <>
       <img src={logo} alt="" />
-      <Form onSubmit={handleSubmit}>
+      <Form ref={formRef} onSubmit={handleSubmit}>
         <Input name="name" placeholder="Nome completo" />
         <Input name="email" type="email" placeholder="Seu e-mail" />
         <Input name="password" type="password" placeholder="Sua senha" />
