1e13b76c6318280a34ffb3ca7d947285b489aed6
JohnAnon9771 <njoao97710@gmail.com>
2020-03-14 01:47:36 -0300

    added styles for notifications

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import logo from '../../assets/logo_purple.svg';
+import Notification from '../Notification';
 
 import { Container, Content, Profile } from './styles';
 
@@ -14,6 +15,7 @@ export default function Header() {
           <Link to="/dashboard">DASHBOARD</Link>
         </nav>
         <aside>
+          <Notification />
           <Profile>
             <div>
               <strong>João Alves</strong>
