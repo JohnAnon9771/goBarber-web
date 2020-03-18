5c06c2c06c7e411c5644b07f2da94cf3d693fe24
JohnAnon9771 <njoao97710@gmail.com>
2020-03-18 00:47:25 -0300

    added style for dashboard and date-fns for data

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,5 +1,55 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+
+import { addDays, subDays, format } from 'date-fns';
+import pt from 'date-fns/locale/pt-BR';
+
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
-  return <div></div>;
+  const [date, setDate] = useState(new Date());
+  const dateFormatted = useMemo(
+    () => format(date, "d 'de' MMMM", { locale: pt }),
+    [date]
+  );
+
+  function handlePrevDate() {
+    setDate(subDays(date, 1));
+  }
+
+  function handleNextDate() {
+    setDate(addDays(date, 1));
+  }
+  return (
+    <Container>
+      <header>
+        <button type="button" onClick={() => handlePrevDate()}>
+          <MdChevronLeft size={32} color="#fff" />
+        </button>
+        <strong>{dateFormatted}</strong>
+        <button type="button" onClick={() => handleNextDate()}>
+          <MdChevronRight size={32} color="#fff" />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>João Alves</span>
+        </Time>
+        <Time available>
+          <strong>08:00</strong>
+          <span>Em aberto</span>
+        </Time>
+        <Time>
+          <strong>08:00</strong>
+          <span>João Alves</span>
+        </Time>
+        <Time>
+          <strong>08:00</strong>
+          <span>João Alves</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
